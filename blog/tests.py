a9a67a8a4cd13dd0bafea68bce191008c013d5c8
sangmandu <45033215+sangmandu@users.noreply.github.com>
2021-08-10 20:31:16 +0900

    포스트 목록 페이지에 태그 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.contrib.auth.models import User
 
 
@@ -23,12 +23,22 @@ class TestView(TestCase):
             name='music',
             slug='music'
         )
+        self.tag_python_kor = Tag.objects.create(
+            name="파이썬 공부", slug="파이썬-공부"
+        )
+        self.tag_python = Tag.objects.create(
+            name="python", slug="python"
+        )
+        self.tag_hello = Tag.objects.create(
+            name="hello", slug="hello"
+        )
         self.post_001 = Post.objects.create(
             title='첫번째 포스트 입니다.',
             content='Hello, World. We are the World.',
             author=self.user_trump,
             category=self.category_programming,
         )
+        self.post_001.tags.add(self.tag_hello)
         self.post_002 = Post.objects.create(
             title='두번째 포스트 입니다.',
             content='안녕 여러분, 나도 여러분의 일부야.',
@@ -40,6 +50,10 @@ class TestView(TestCase):
             content='카테고리 없어.',
             author=self.user_obama,
         )
+        self.post_003.tags.add(self.tag_python_kor)
+        self.post_003.tags.add(self.tag_python)
+
+
 
     def navbar_Test(self, soup):
         navbar = soup.nav
@@ -106,14 +120,23 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title, main_area.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, main_area.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertNotIn(self.tag_python.name, post_002_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_002_card.text)
 
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn(self.post_003.title, main_area.text)
         self.assertIn('미분류', post_003_card.text)
+        self.assertNotIn(self.tag_hello.name, post_003_card.text)
+        self.assertIn(self.tag_python.name, post_003_card.text)
+        self.assertIn(self.tag_python_kor.name, post_003_card.text)
 
         self.assertIn(self.post_001.author.username.upper(), main_area.text)
         self.assertIn(self.post_002.author.username.upper(), main_area.text)
